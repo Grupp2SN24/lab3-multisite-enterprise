0e0f2e04d75525aa5a7afa33fbcef0d4484379dd
Anton <ant-life@hotmail.com>
2025-12-04 21:48:13 +0100

    Add persistent routes + Branch-A thin client configs

--- configs/dc/services/terminal-2/etc/network-config.sh
+++ configs/dc/services/terminal-2/etc/network-config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Terminal-2 Network Configuration (AlmaLinux 9.4)
 nmcli con add type ethernet con-name "services" ifname eth0 ipv4.addresses 10.10.0.32/24 ipv4.method manual autoconnect yes
+nmcli con mod "services" +ipv4.routes "10.20.1.0/24 10.10.0.1, 10.20.2.0/24 10.10.0.1, 10.0.0.0/24 10.10.0.1"
 nmcli con up "services"
